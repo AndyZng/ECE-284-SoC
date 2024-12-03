e288a758474c33f19e02ecdcab09ea978847b853
David Wang <davidwang334@gmail.com>
2024-12-02 20:50:53 -0800

    l0

--- part2/verilog/l0_version1.v
+++ part2/verilog/l0_version1.v
@@ -20,39 +20,35 @@ module l0 (clk, in, out, rd, wr, o_full, reset, o_ready);
   
   genvar i;
 
-  assign o_ready = ~&full;
-  assign o_full  = &full;
+  assign o_ready = ~(|full);
+  assign o_full  = |full;
 
 
   for (i=0; i<row ; i=i+1) begin : row_num
       fifo_depth64 #(.bw(bw)) fifo_instance (
-	 .rd_clk(clk),
-	 .wr_clk(clk),
-	 .rd(rd_en[i]),
-	 .wr(wr),
-         .o_empty(empty[i]),
-         .o_full(full[i]),
-	 .in(in[bw*(i+1)-1:bw*i]),
-	 .out(out[bw*(i+1)-1:bw*i]),
-         .reset(reset));
-  end
+        .rd_clk(clk),
+        .wr_clk(clk),
+        .rd(rd_en[i]),
+        .wr(wr),
+        .o_empty(empty[i]),
+        .o_full(full[i]),
+        .in(in[(i+1)*bw-1:i*bw]),
+        .out(out[(i+1)*bw-1:i*bw]),
+        .reset(reset)
+      );
+    end
 
 
   always @ (posedge clk) begin
    if (reset) begin
       rd_en <= 8'b00000000;
    end
-   else
-
-      /////////////// version1: read all row at a time ////////////////
-      rd_en <= {row{rd}};
-      ///////////////////////////////////////////////////////
-
-
-
-      //////////////// version2: read 1 row at a time /////////////////
-      // rd_en <= {rd_en[row-2:0], rd};
-      ///////////////////////////////////////////////////////
+   else begin
+    
+        /////////////// version1: read all row at a time ////////////////
+        rd_en <= {row{rd}};
+        ///////////////////////////////////////////////////////
     end
+  end
 
 endmodule
